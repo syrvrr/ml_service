37a0c5a73ab150250ba112d88771107c5ca2e772
sayray303 <andi.prayogi30@gmail.com>
2021-07-16 23:04:48 +0700

    add predict view

--- backend/server/endpoints/urls.py
+++ backend/server/endpoints/urls.py
@@ -7,6 +7,8 @@ from endpoints.views import MLAlgorithmViewSet
 from endpoints.views import MLAlgorithmStatusViewSet
 from endpoints.views import MLRequestViewSet
 
+from endpoints.views import PredictView # import PredictView
+
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
@@ -15,4 +17,8 @@ router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
 
 urlpatterns = [
     url(r"^api/v1/", include(router.urls)),
+    # add predict url
+    url(
+        r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+    ),
 ]
